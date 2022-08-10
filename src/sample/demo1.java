211bbb963c12ef4b5435a4495f9f99695af7dece
Shree <Shree@DESKTOP-CJCAR7T>
2022-08-10 22:58:56 +0530

    [dnyaneshwar]: 1st changes in our project.

---
+++ src/sample/demo1.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class demo1 
+{
+	
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		//url
+		//https://github.com/dnyaneshwar26/GITHUB_project.git
+		//token
+		//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
+		
+	}
+
+}
---
+++ src/sample/demo2.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class demo2 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("all is well");
+		
+	}
+
+}
